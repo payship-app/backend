652e74c4b82a729b9a58475525a5b7fc4af4f25a
payship-app <mypayship@gmail.com>
2026-01-16 13:24:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const jwt = require("jsonwebtoken");
 const User = require("./models/User");
 const Blog = require("./models/Blog");
 const upload = require("./multer"); // multer configured for Cloudinary
+const Blog = require("./models/Blog");
+
 
 // Create Express app
 const app = express();
@@ -108,3 +110,4 @@ app.post("/api/create-post", upload.single("image"), async (req, res) => {
 // ------------------------
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
